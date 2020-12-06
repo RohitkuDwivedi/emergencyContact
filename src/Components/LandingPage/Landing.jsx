852199aa95db61a7ba5c1569416b2868cae9f615
RohitKuDwivedi <rdwivedi345@gmail.com>
2020-12-07 00:14:08 +0530

    my layout done

--- src/Components/LandingPage/Landing.jsx
+++ src/Components/LandingPage/Landing.jsx
@@ -1,15 +1,55 @@
 import React, { Component } from "react";
 import face from "../../Images/moneyHeist.png";
 import "./Landing.css";
+import data from "../../Data/db";
 
 class LandingPage extends Component {
-  state = {};
+  state = { rohit: "" };
+
+  componentDidMount() {
+    this.setState({
+      rohit: data.getMyDetails(),
+      groups: data.getAllGroups(),
+    });
+  }
   render() {
+    console.log(this.state);
     return (
-      <div className="lp_box">
-        <div className="profileImageName">
+      <div className="lp_box col-md-12 col-12">
+        <div className="profileImageName col-sm-12 col-md-4">
           <img className="profPic" src={face} alt="Profile Pic" />
-          <h2>Rohit Kumar Dwivedi</h2>
+          <h3>
+            <a href={"tel:" + this.state.rohit.phone}>
+              {this.state.rohit.fname + " " + this.state.rohit.lname + " "}
+              <span className="glyphicon"> &#xe182;</span>
+            </a>
+          </h3>
+        </div>
+        <div className="profileImageName col-sm-12 col-md-4">
+          <div>
+            <h3>Emergency Contact</h3>
+            {this.state.rohit !== "" ? (
+              <h3 style={{ display: "flex", justifyContent: "space-between" }}>
+                {this.state.rohit.contact[0].fname + " "}
+                <h4>
+                  <a
+                    className="glyphicon"
+                    href={"tel:" + this.state.rohit.contact[0].phone[0]}
+                  >
+                    &#xe182;1
+                  </a>
+                </h4>
+                <h4>
+                  <a
+                    className="glyphicon"
+                    href={"tel:" + this.state.rohit.contact[0].phone[1]}
+                  >
+                    &#xe182;2
+                  </a>
+                </h4>
+              </h3>
+            ) : null}
+          </div>
         </div>
       </div>
     );
--- src/Data/db.js
+++ src/Data/db.js
@@ -1,29 +1,67 @@
-Data = {
-    "hirmi": [
-        {
-            fname: "kritika",
-            lname: "raina",
-            phone: 9898879830,
-            address: "hirmi township",
+class data {
+    constructor() {
+        this.me = {
+            fname: "Rohit",
+            lname: "Kumar Dwivedi",
+            phone: "9977770648",
             contact: [
                 {
-                    fname: "arti",
-                    lname: "raina",
-                    relation: "mother",
-                    phone: 9898879830,
-                    address: "E-143,hirmi,township",
+                    fname: "Deepak",
+                    lname: "kumar",
+                    phone: ["+919803470060", "+919315807329"],
+                    address: ""
                 },
                 {
-                    fname: "kuldeep",
-                    lname: "raina",
-                    relation: "father",
-                    phone: 9826639780,
-                    address: "E-143,hirmi,township",
+                    fname: "Deepak",
+                    lname: "kumar",
+                    phone: ["+919803470060", "+919315807329"],
+                    address: ""
                 }
             ]
         }
-    ],
-    "college": [],
-    "family": []
+        this.allData = {
+            "hirmi": [
+                {
+                    fname: "kritika",
+                    lname: "raina",
+                    phone: 9898879830,
+                    address: "hirmi township",
+                    contact: [
+                        {
+                            fname: "arti",
+                            lname: "raina",
+                            relation: "mother",
+                            phone: 9898879830,
+                            address: "E-143,hirmi,township",
+                        },
+                        {
+                            fname: "kuldeep",
+                            lname: "raina",
+                            relation: "father",
+                            phone: 9826639780,
+                            address: "E-143,hirmi,township",
+                        }
+                    ]
+                }
+            ],
+            "college": [],
+            "family": []
+
+        }
+
+    }
+    getAllGroups() {
+        let allGroups = {}
+        Object.keys(this.allData).forEach(key => {
+            key = key.toString()
+            allGroups[key] = this.allData[key].length
+        });
+        return allGroups
+    }
+
+    getMyDetails() {
+        return this.me
+    }
 
 }
+export default data = new data()
